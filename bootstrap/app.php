d4ad7d9a914ab8a613d8237b891314c1e801bfb0
Junior Fontenele <git@juniorfontenele.com.br>
2026-02-12 11:51:12 -0300

    Register Laravel Tracing middlewares

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use JuniorFontenele\LaravelTracing\Middleware\IncomingTracingMiddleware;
+use JuniorFontenele\LaravelTracing\Middleware\OutgoingTracingMiddleware;
 use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -30,6 +32,8 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->web(append: [
             SetUserLocale::class,
+            IncomingTracingMiddleware::class,
+            OutgoingTracingMiddleware::class,
             AddContextToSentry::class,
             AddSecurityHeaders::class,
             HandleAppearance::class,
